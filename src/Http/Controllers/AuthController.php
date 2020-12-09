844caa9735efce3bebd2fb5a190c44a47d2b94e1
thanhtamtotaa <thanhtamtotaa@gmail.com>
2020-12-09 17:41:46 +0700

    Create AuthController.php

---
+++ src/Http/Controllers/AuthController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Totaa\Auth\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Auth;
+use Totaa\Auth\DataTables\AdminAuthDataTable;
+
+class TeamController extends Controller
+{
+    /**
+     * index
+     *
+     * @return void
+     */
+    public function index(AdminAuthDataTable $dataTable)
+    {
+        if (Auth::user()->bfo_info->hasAnyPermission(["view-account"])) {
+            return $dataTable->render('totaa-account::account', ['title' => 'Quản lý Tài khoản']);
+        } else {
+            return view('errors.dynamic', [
+                'error_code' => '403',
+                'error_description' => 'Không có quyền truy cập',
+                'title' => 'Quản lý Tài khoản',
+            ]);
+        }
+    }
+}
