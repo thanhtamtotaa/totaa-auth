a201587bb733216f7ad424093a0af50690cfae92
thanhtamtotaa <thanhtamtotaa@gmail.com>
2020-12-09 18:18:06 +0700

    Create style.blade.php

---
+++ resources/views/livewire/support/style.blade.php
@@ -0,0 +1,8 @@
+<style>
+    [totaa-datatables]#bfoinfo-table > thead > tr > th:nth-child(999),
+    [totaa-datatables]#bfoinfo-table > tbody > tr > td:nth-child(999) {
+        overflow: hidden;
+        text-overflow: ellipsis;
+        max-width: 230px;
+    }
+</style>
