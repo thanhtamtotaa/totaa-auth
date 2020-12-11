b1e8fe3665b4a890b9552478d8d57bd20d76c00f
thanhtamtotaa <thanhtamtotaa@gmail.com>
2020-12-11 12:05:30 +0700

    Update account-livewire.blade.php

--- resources/views/livewire/account-livewire.blade.php
+++ resources/views/livewire/account-livewire.blade.php
@@ -1,9 +1,15 @@
+@php
+    $bfo_info_arrays = Totaa\TotaaBfo\Models\BfoInfo::select("mnv", "full_name")->get()->toArray();
+@endphp
+
 <div>
     <!-- Filters and Add Buttons -->
     @include('totaa::livewire.support.filters')
 
     <!-- Incluce các modal -->
     @include('totaa::livewire.modal.add_edit_modal')
+    @include('totaa::livewire.modal.link_modal')
+    @include('totaa::livewire.modal.reset_modal')
 
     <!-- Scripts -->
     @push('livewires')
