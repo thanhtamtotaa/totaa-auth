81a3455d7616c0ec0aa588515eecd2082974251d
thanhtamtotaa <thanhtamtotaa@gmail.com>
2020-12-11 12:05:41 +0700

    Update AdminAuthDataTable.php

--- src/DataTables/AdminAuthDataTable.php
+++ src/DataTables/AdminAuthDataTable.php
@@ -8,6 +8,7 @@ use Yajra\DataTables\Html\Column;
 use Yajra\DataTables\Html\Editor\Editor;
 use Yajra\DataTables\Html\Editor\Fields;
 use Yajra\DataTables\Services\DataTable;
+use Auth;
 
 class AdminAuthDataTable extends DataTable
 {
@@ -21,7 +22,42 @@ class AdminAuthDataTable extends DataTable
     {
         return datatables()
             ->eloquent($query)
-            ->addColumn('action', 'user.action');
+            ->addColumn('action', function ($query) {
+                $Action_Icon="<div class='action-div icon-4 px-0 mx-1 d-flex justify-content-around text-center'>";
+
+                if (Auth::user()->bfo_info->can("edit-account")) {
+                    $Action_Icon.="<div class='col action-icon-w-50 action-icon' totaa-edit-account='$query->id'><i class='text-twitter fas fa-user-edit'></i></div>";
+
+                    $Action_Icon.="<div class='col action-icon-w-50 action-icon' totaa-link-account='$query->id'><i class='text-success fas fa-link'></i></div>";
+
+                    $Action_Icon.="<div class='col action-icon-w-50 action-icon' totaa-reset-password='$query->id'><i class='text-linux fas fa-user-lock'></i></div>";
+                }
+
+                $Action_Icon.="</div>";
+
+                return $Action_Icon;
+            })
+            ->editColumn('bfo_info.mnv', function ($query) {
+                if (!!optional($query->bfo_info)->mnv) {
+                    return "[".optional($query->bfo_info)->mnv ."] ".optional($query->bfo_info)->full_name;
+                } else {
+                    return NULL;
+                }
+            })
+            ->editColumn('created_at', function ($query) {
+                if (!!$query->created_at) {
+                    return $query->created_at->format("d-m-Y H:i");
+                } else {
+                    return NULL;
+                }
+            })
+            ->editColumn('deleted_at', function ($query) {
+                if (!!$query->deleted_at) {
+                    return "Đã xóa";
+                } else {
+                    return "Đang hoạt động";
+                }
+            });
     }
 
     /**
@@ -32,7 +68,13 @@ class AdminAuthDataTable extends DataTable
      */
     public function query(User $model)
     {
-        return $model->newQuery();
+        $query = $model->newQuery()->withTrashed();
+
+        if (!request()->has('order')) {
+            $query->orderBy('id', 'desc');
+        };
+
+        return $query->with(["bfo_info:mnv,full_name"]);
     }
 
     /**
@@ -45,9 +87,25 @@ class AdminAuthDataTable extends DataTable
         return $this->builder()
                     ->setTableId('user-table')
                     ->columns($this->getColumns())
-                    ->minifiedAjax()
-                    ->dom('frtip')
-                    ->orderBy(1);
+                    ->dom("<'row'<'col-md-6 col-sm-12'l><'col-md-6 col-sm-12'f>r><'row'<'col-sm-12 table-responsive't>><'row'<'col-md-5 col-sm-12'i><'col-md-7 col-sm-12'p>>")
+                    ->parameters([
+                        "autoWidth" => false,
+                        "lengthMenu" => [
+                            [10, 25, 50, -1],
+                            [10, 25, 50, "Tất cả"]
+                        ],
+                        "order" => [],
+                        'initComplete' => 'function(settings, json) {
+                            var api = this.api();
+                            window.addEventListener("dt_draw", function(e) {
+                                api.draw(false);
+                                e.preventDefault();
+                            })
+                            api.buttons()
+                                .container()
+                                .appendTo($("#datatable-button"));
+                        }',
+                    ]);
     }
 
     /**
@@ -59,14 +117,61 @@ class AdminAuthDataTable extends DataTable
     {
         return [
             Column::computed('action')
-                  ->exportable(false)
-                  ->printable(false)
-                  ->width(60)
-                  ->addClass('text-center'),
-            Column::make('id'),
-            Column::make('add your columns'),
-            Column::make('created_at'),
-            Column::make('updated_at'),
+                    ->exportable(false)
+                    ->printable(false)
+                    ->width(60)
+                    ->addClass('text-center')
+                    ->title("")
+                    ->footer(""),
+            Column::make('id')
+                  ->title("ID")
+                  ->addClass('text-center')
+                  ->width(5)
+                  ->searchable(true)
+                  ->orderable(true)
+                  ->footer("ID"),
+            Column::make("name")
+                  ->title("Họ tên")
+                  ->width(200)
+                  ->searchable(true)
+                  ->orderable(true)
+                  ->footer("Họ tên"),
+            Column::make("email")
+                  ->title("Email")
+                  ->width(150)
+                  ->searchable(true)
+                  ->orderable(false)
+                  ->footer("Email"),
+            Column::make("phone")
+                  ->title("Số điện thoại")
+                  ->width(50)
+                  ->searchable(true)
+                  ->orderable(false)
+                  ->footer("Số điện thoại"),
+            Column::make("bfo_info.mnv")
+                  ->title("Liên kết với nhân viên")
+                  ->width(200)
+                  ->searchable(true)
+                  ->orderable(false)
+                  ->render("function() {
+                        if (!!data) {
+                            return data;
+                        } else {
+                            return null;
+                        }
+                    }")
+                  ->footer("Liên kết với nhân viên"),
+            Column::computed("deleted_at")
+                  ->title("Trạng thái")
+                  ->searchable(true)
+                  ->width(20)
+                  ->orderable(true)
+                  ->footer("Trạng thái"),
+            Column::computed("created_at")
+                  ->title("Thời gian đăng ký")
+                  ->searchable(false)
+                  ->orderable(false)
+                  ->footer("Thời gian đăng ký"),
         ];
     }
 
